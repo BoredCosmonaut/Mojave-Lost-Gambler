3d4fbb699419a9128af9830c02b0a2c419d8de56
Berk <berk5681@outlook.com>
2026-04-13 23:36:45 +0300

    somehow the corse was not in package json

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,8 +3,9 @@ dotenv.config();
 
 import app from './app.js';
 
-const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Live on Port ${PORT}`);
 });
