bedb0509a9ca8026da6229057708a88de3ddd71a
Berk <berk5681@outlook.com>
2026-04-13 23:57:05 +0300

    forgot to do the cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -11,7 +11,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const app = express();
 app.use(cors({
-  origin: "http://localhost:8082", 
+  origin: "http://localhost:8080", 
   credentials: true
 }));
 
--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,20 +1,19 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL:process.env.VUE_APP_API_BASE_URL,
-    withCredentials:true
+
+    baseURL: process.env.VUE_APP_API_BASE_URL,
+    withCredentials: true
 });
 
 api.interceptors.request.use((config) => {
     const token = localStorage.getItem('mojave_access_key');
-    console.log("AXIOS_SENDING_TOKEN:", token);
     if(token) {
         config.headers.Authorization = `Bearer ${token}`;
     }
-
-    return config
+    return config;
 }, (err) => {
-    return Promise.reject(err)
+    return Promise.reject(err);
 });
 
 export default api;
